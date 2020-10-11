b84206e7e6a2e93c0c8d25d0f79f6ada9f26d239
James Solomon <solomonjames@spotify.com>
2020-10-11 15:37:26 -0400

    Fixing stylesheet

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-        <link type="text/css" href="/css/app.css" />
+        <link rel="stylesheet" href="/css/app.css" />
     </head>
 
     <body>
