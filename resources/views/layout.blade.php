8262c844905b501f2eee49767c084fcc0f586b26
James Solomon <solomonjames@spotify.com>
2020-10-10 20:00:24 -0400

    Updates to layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,3 +1,15 @@
-<div class="lg:container lg:mx-auto">
-    @section('content')@endsection
-</div>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+
+        <link type="text/css" href="/css/app.css" />
+        <script type="application/javascript" src="/js/app.js"></script>
+    </head>
+
+    <body>
+        <div class="lg:container lg:mx-auto">
+            @yield('content')
+        </div>
+    </body>
+</html>
