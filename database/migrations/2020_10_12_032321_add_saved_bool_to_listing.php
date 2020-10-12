8d5970a590d75b0e8c914216f3e4bd7ebd2fe4f0
James Solomon <solomonjames@spotify.com>
2020-10-11 23:34:25 -0400

    scerwed up migration

--- database/migrations/2020_10_12_032321_add_saved_bool_to_listing.php
+++ database/migrations/2020_10_12_032321_add_saved_bool_to_listing.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSavedBoolToListings extends Migration
+class AddSavedBoolToListing extends Migration
 {
     /**
      * Run the migrations.
