5e64adbe8cd12827f5cac83787efc6e5f279b101
James Solomon <solomonjames@spotify.com>
2020-10-24 16:49:44 -0400

    trying something to fix issue

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Search from './Search';
+// import Search from './Search';
 import ListingActions from "./ListingActions";
 
 require('./bootstrap');
 
-ReactDOM.render(
-    <React.StrictMode>
-        <Search />
-    </React.StrictMode>,
-    document.getElementById('searcher')
-);
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <Search />
+//     </React.StrictMode>,
+//     document.getElementById('searcher')
+// );
 
 ReactDOM.render(
     <React.StrictMode>
