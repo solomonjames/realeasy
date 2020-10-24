c027849fd33206b749288fdeb00028403803199c
James Solomon <solomonjames@spotify.com>
2020-10-24 17:01:59 -0400

    Looping each item

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Search from './Search';
+import Search from './Search';
 import ListingActions from "./ListingActions";
 
 require('./bootstrap');
 
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <Search />
-//     </React.StrictMode>,
-//     document.getElementById('searcher')
-// );
-
 ReactDOM.render(
     <React.StrictMode>
-        <ListingActions />
+        <Search />
     </React.StrictMode>,
-    document.getElementsByClassName('listing_actions')
+    document.getElementById('searcher')
 );
+
+const listingActions = document.getElementsByClassName('listing_actions');
+for (let i in listingActions) {
+    ReactDOM.render(
+        <React.StrictMode>
+            <ListingActions />
+        </React.StrictMode>,
+        listingActions[i]
+    );
+}
